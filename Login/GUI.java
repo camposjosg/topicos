353f42c27ddbac2d04fa8e1a356ffbf3d7a9ae96
camposjosg <irjosgutierrez@gmail.com>
2022-10-08 18:50:27 -0500

    0.5

--- Login/GUI.java
+++ Login/GUI.java
@@ -1,25 +1,32 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event;
 
 public class GUI {
 
     public static void main (String[]args){
 
         JButton btn1 = new JButton("Ingresar");
-        JToggleButton btn2 = new JToggleButton("Como estan?");
-        JLabel etiqueta = new JLabel("Saludos y");
-        JTextField campo = new JTextField("Adios..", 12);
-        JFrame marco = new JFrame();
-        marco.setTitle("FlowLayout(int align, int hgap, int vgap)");
-        marco.setSize(400,300);
-        marco.setLocationRelativeTo(null);
-        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        marco.setVisible(true);
+        JButton btn2 = new JButton("Crear usuario");
+        JLabel etiqueta1 = new JLabel("IrvBook");
+        JLabel etiqueta2 = new JLabel("¿Listo para navegar?");
+        //JTextField campo = new JTextField("Adios..", 12);
+        JFrame ventana = new JFrame();
+
+        ventana.setTitle("IrvBook");
+        ventana.setSize(400,300);
+        ventana.setLocationRelativeTo(null);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+        
+        ventana.setLayout(new FlowLayout(FlowLayout.LEFT, 150,20) );
        
-        marco.setLayout(new FlowLayout(FlowLayout.LEFT, 150,20) );
-       
-        marco.add(btn1);marco.add(btn2);
-        marco.add(etiqueta);marco.add(campo);
+        ventana.add(etiqueta1);
+        ventana.add(etiqueta2);
+        ventana.add(btn1);
+        ventana.add(btn2);
+        
 
     }
+
 }
---
+++ Prueba.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Prueba extends JFrame implements ActionListener {
+
+JButton boton1;
+
+  public Prueba() {
+
+    //Layout absoluto
+    setLayout(null);
+
+    //Tamaño de la ventana
+    setBounds((0,0,450,350);
+
+    //Título
+    setTitle("Ejemplo 1: Botón");
+
+    //No redimensionable
+    setResizable(false);
+
+    //Cerrar proceso al cerrar la ventana
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    //Botón
+    boton1=new JButton("Finalizar");
+    boton1.setBounds(300,250,100,30);
+    add(boton1);
+    boton1.addActionListener(this);
+
+    //Muestro JFrame (lo último para que lo pinte todo correctamanete)
+    setVisible(true);
+
+
+  }
+
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == boton1) {
+    System.exit(0);
+    }
+  }
+
+  public static void main(String[] args) {
+    new Prueba();
+  }
+}
+
