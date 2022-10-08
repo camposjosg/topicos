55c0e504afa97d7e0d3333384cab8d90e4d6b197
camposjosg <irjosgutierrez@gmail.com>
2022-10-07 19:34:26 -0500

    v0.2 En ésta edición ya está listo los botones que nos abriran ventanas para guardar la documentación

--- Login/GUI.java
+++ Login/GUI.java
@@ -1,40 +1,25 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import java.awt.*;
+import javax.swing.*;
 
 public class GUI {
 
     public static void main (String[]args){
 
-        
-
-        JFrame ventana = new JFrame("Login para apps");
-        JButton panelN = new JButton("Norte");
-        JButton panelS = new JButton("Sur");
-        JButton panelE = new JButton("Este");
-        JLabel panelO = new JLabel("Oeste");
-        JLabel panelC = new JLabel("Centro");
-        JButton panelON = new JButton("Oeste Norte");
-
-        panelO.setBackground(Color.BLUE);
-        panelO.add(panelON, BorderLayout.NORTH);
-
-        //ventana.add(panelN, BorderLayout.NORTH);
-        //ventana.add(panelS, BorderLayout.SOUTH);
-        //ventana.add(panelE, BorderLayout.EAST);
-        ventana.add(panelO, BorderLayout.WEST);
-        ventana.add(panelC, BorderLayout.CENTER);
-
-        
-
-
-        ventana.setSize(500, 500);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setVisible(true);
-        ventana.setResizable(false);
+        JButton btn1 = new JButton("Ingresar");
+        JToggleButton btn = new JToggleButton("Como estan?");
+        JLabel etiqueta = new JLabel("Saludos y");
+        JTextField campo = new JTextField("Adios..", 12);
+        JFrame marco = new JFrame();
+        marco.setTitle("FlowLayout(int align, int hgap, int vgap)");
+        marco.setSize(400,300);
+        marco.setLocationRelativeTo(null);
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setVisible(true);
+       
+        marco.setLayout(new FlowLayout(FlowLayout.LEFT, 150,0) );
+       
+        marco.add(btn1);marco.add(btn);
+        marco.add(etiqueta);marco.add(campo);
 
     }
 }
